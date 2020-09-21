1087b22e682c6ec5c4dffe98a1feafb51613c696
sandhya23ram <47956751+sandhya23ram@users.noreply.github.com>
2020-09-20 20:58:17 -0700

    Create helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,15 @@
+def main():
+  print ("\nhello world!\n")
+
+
+if __name__ == "__main__":
+  main()
+
+
+#OR
+
+#def hello():
+#    print ('Sri Ramajayam')
+
+#if __name__ == "__main__":
+#    hello()
