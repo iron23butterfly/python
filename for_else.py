96d1660d66ccc2a3fe02abb0ef4bedb09d6a8aae
iron23butterfly <47956751+iron23butterfly@users.noreply.github.com>
2020-09-25 05:58:39 -0700

    Create for_else.py

---
+++ for_else.py
@@ -0,0 +1,15 @@
+animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
+
+for pet in animals:
+    if pet == 'dog':
+        continue
+    if pet == 'velociraptor':
+        break
+    print(pet)
+else:
+    print ('this is all of the animals!!')
+
+# OUTPUT:
+# bear
+# bunny
+# cat
