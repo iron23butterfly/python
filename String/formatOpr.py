d55df37f7ce370ef3ddfadcbc3fa4d287e885891
iron23butterfly <47956751+iron23butterfly@users.noreply.github.com>
2020-09-25 05:14:00 -0700

    Update formatOpr.py

--- String/formatOpr.py
+++ String/formatOpr.py
@@ -9,6 +9,14 @@ print('|7 is "{1:>09}" "{0:010}"|'.format(x, y))
  # Another way to do this 
 print(f'|7 is "{y:>09}" "{x:010}"|')
 
+x = 0x0a
+y = 0x02
+z = x & y
+
+print(f'(hex) x is {x:02x}, y is {y:02x}, z is {z:02x}')
+print(f'(bin) x is {x:08b}, y is {y:08b}, z is {z:08b}')
+
+
 # OUTPUT:
 # |1 is 8 9|
 # |2 is 9 8|
@@ -18,3 +26,7 @@ print(f'|7 is "{y:>09}" "{x:010}"|')
 # |6 is "000000019" "0000000043"|
 # |7 is "000000043" "0000000019"|
 # |7 is "000000043" "0000000019"|
+
+# OUTPUT
+# (hex) x is 0a, y is 02, z is 02
+# (bin) x is 00001010, y is 00000010, z is 00000010
