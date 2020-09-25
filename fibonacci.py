fd0e5a0c4e4cc97997b9b9fe677ca5ef99bb60a5
iron23butterfly <47956751+iron23butterfly@users.noreply.github.com>
2020-09-24 19:34:20 -0700

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+# simple fibonacci series
+# the sum of two elements defines the next set
+
+a, b = 0, 1
+while b < 1000:
+    print(b, end = ' ', flush = True)
+    a, b = b, a + b
+
+print() # line ending
+
+# OUTPUT
+# 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 
