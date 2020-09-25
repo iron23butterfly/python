f05eb599c46c8aa46a41e15fe662e9b286d8b92b
iron23butterfly <47956751+iron23butterfly@users.noreply.github.com>
2020-09-24 18:03:47 -0700

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,10 @@
 def main():
+  print ('\n')
+  print ('\nJaiShriRam')
+  user_name = input ('Wht is your name? ')
+  print ('RamRam ', user_name,"!!")
+  print ('\n' )
+  print ("\nGaneshaay Namaha!\n")
   print ("\nhello world!\n")
 
 
