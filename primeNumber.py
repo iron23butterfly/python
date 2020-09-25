0828b3752a28736a3407d88bb8a1838dcca289ad
iron23butterfly <47956751+iron23butterfly@users.noreply.github.com>
2020-09-24 19:50:17 -0700

    Update primeNumber.py

--- primeNumber.py
+++ primeNumber.py
@@ -6,12 +6,19 @@ def isprime(n):
             return False
     else:
         return True
+    
+def list_all_prime(maxLimit):
+    for i in maxLimit:
+        if isprime(i):
+            print(i)
 
 n = 5
 if isprime(n):
     print(f'{n} is prime')
 else:
     print(f'{n} not prime')
+    
+
 
 # OUTPUT
 # 5 is prime
