3eba0e370d81e3a6480a951fea660c37b52a178d
iron23butterfly <47956751+iron23butterfly@users.noreply.github.com>
2020-09-25 19:47:13 -0700

    Create inclusiveRangeGenerator.py

---
+++ Functions/inclusiveRangeGenerator.py
@@ -0,0 +1,33 @@
+def main():
+    for i in inclusive_range(25):
+        print(i, end = ' ')
+    print()
+
+def inclusive_range(*args):
+    numargs = len(args)
+    start = 0
+    step = 1
+    
+    # initialize parameters
+    if numargs < 1:
+        raise TypeError(f'expected at least 1 argument, got {numargs}')
+    elif numargs == 1:
+        stop = args[0]
+    elif numargs == 2:
+        (start, stop) = args
+    elif numargs == 3:
+        (start, stop, step) = args
+    else: raise TypeError(f'expected at most 3 arguments, got {numargs}')
+
+    # generator
+    i = start
+    while i <= stop:
+        yield i
+        i += step
+
+
+if __name__ == '__main__':
+    main()
+
+# OUTPUT
+# 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
