a7d232e5640c4828c4304984db1bb651eb576b48
iron23butterfly <47956751+iron23butterfly@users.noreply.github.com>
2020-09-26 23:18:07 -0700

    Update basic_00.py

--- Exceptions/basic_00.py
+++ Exceptions/basic_00.py
@@ -6,7 +6,17 @@ def main():
     except:
         print('Spend you time learning or praying!!')
 
+    try:
+        x = str('foo')
+    except ValueError:
+        print('I caught an error using exceptions!')
+    except:
+        print('Spend you time learning or praying!!')
+    else:
+        print ('Good job - No eerors')
+
 if __name__ == '__main__': main()
 
 # OUTPUT
 # I caught an error using exceptions!
+# Good job - No eerors
