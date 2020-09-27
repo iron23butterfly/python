cac49f29645a2b5e818144f03a5a49ecbb41ce78
iron23butterfly <47956751+iron23butterfly@users.noreply.github.com>
2020-09-27 06:51:44 -0700

    Create multiLineString.py

---
+++ String/multiLineString.py
@@ -0,0 +1,26 @@
+print('''
+      Hello, 
+      World.
+      This 
+      is a 
+      multi-line
+      comment
+      ''')
+print("""
+      \n
+      This 
+      is an
+      alternate
+      """)
+
+# OUTPUT
+# Hello,
+# World.
+# This
+# is a
+# multi - line
+# comment
+# 
+# This
+# is an
+# alternate
